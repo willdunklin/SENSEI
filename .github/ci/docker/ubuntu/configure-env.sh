07215d179345dbd06b6f3a740a8789bf2f8132ce
Will Dunklin <will.dunklin@kitware.com>
2023-04-30 16:34:09 +0000

    Change order of ops in config-env

--- .github/ci/docker/ubuntu/configure-env.sh
+++ .github/ci/docker/ubuntu/configure-env.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 . ${SPACK_ROOT}/share/spack/setup-env.sh
 
 if [[ -z ${SENSEI_ENV} ]]; then
@@ -10,9 +11,9 @@ export CMAKE_CXX_COMPILER_LAUNCHER=ccache
 export OMPI_ALLOW_RUN_AS_ROOT=1
 export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 
-module use ${SPACK_ROOT}/share/spack/lmod/linux-ubuntu20.04-x86_64/Core
-
-module load openmpi ninja swig
 spack env activate ${SENSEI_ENV}
+
+module use ${SPACK_ROOT}/share/spack/lmod/linux-ubuntu20.04-x86_64/Core
+module load openmpi ninja swig
 
 . ${SPACK_ROOT}/var/spack/environments/${SENSEI_ENV}/loads
